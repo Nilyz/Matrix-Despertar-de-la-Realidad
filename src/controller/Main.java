241ac101f7e2cf8b713a891923d675101d65b0d9
Yilin <yilin.zh135@gmail.com>
2024-03-01 00:50:42 +0100

    Fix fight method

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -3,9 +3,12 @@ package controller;
 import model.*;
 import static model.Zone.*;
 
+import java.util.Scanner;
+
 public class Main {
 	public static void main(String[] args) {
-		
+		Scanner sc= new Scanner(System.in);
+
 		
 		
 		Player jugador= new Player();
@@ -20,9 +23,18 @@ public class Main {
 		zone1.verZona();
 		
 		Battle lucha= new Battle();
-		lucha.fight(jugador, zone1.getAgents().get(0));
+		lucha.menuBattle();
+		for(int i=0;i<zone1.getAgents().size();i++) {
+			int option;
+			//que me siga preguntando que hacer mientars que el enemigo o jugador sigan vivos
+			while(jugador.getHealth()>0 && zone1.getAgents().get(i).getHealth()>0) {
+				option=sc.nextInt();
+				lucha.battleOptions(option,jugador, zone1.getAgents().get(i));
+			}
+			
+		}
 		
-	
+		
 	}
 
 }
--- src/model/Battle.java
+++ src/model/Battle.java
@@ -1,42 +1,70 @@
 package model;
 
+import java.util.Scanner;
+
 public class Battle {
 
-    public static void fight(Player player, Agent agent) {
-        System.out.println("Fuiste descubierto por un Agente, prepárate para el enfrentamiento!");
-        
-        boolean playerDefeated = false;
-        boolean enemyDefeated = false;
-        
-        while (!playerDefeated && !enemyDefeated) {
-            int playerDamage = player.getStrength();
-            int enemyDamage = agent.getStrength();
-            
-            // Turno del jugador
-            int enemyHP = Math.max(0, agent.getHealth() - playerDamage);
-            agent.setHealth(enemyHP);
-            System.out.println(player.getName() + " ataca al enemigo y causa " + playerDamage + " de daño.");
-            System.out.println("El enemigo tiene " + enemyHP + " de salud.");
+	public static void menuBattle() {
+		System.out.println("Elige una opción:");
+		System.out.println("1. Atacar");
+		System.out.println("2. Usar objetos");
+		System.out.println("3. Huir");
+		System.out.println("4. Ver stats");
+		
+	}
 
-            // ver si se murió el enemigo
-            if (enemyHP <= 0) {
-                System.out.println("¡El enemigo ha sido derrotado!");
-                enemyDefeated = true;
-            } else {
-                // Turno del enemigo
-                int playerHP = Math.max(0, player.getHealth() - enemyDamage);
-                player.setHealth(playerHP);
-                System.out.println("El enemigo te ataca y causa " + enemyDamage + " de daño.");
-                System.out.println("Tienes " + playerHP + " de salud.");
+	
+	public static void battleOptions(int option,Player player, Agent agent) {
+		
+		switch(option) {
+		case 1:
+			fight(player, agent);
+			break;
+		case 2:
+			break;
+		case 3:
+			break;
+		case 4:
+			break;
+		}
+	}
+	
+	
+	public static void fight(Player player, Agent agent) {
 
-                // Ver si se murió el jugador
-                if (playerHP <= 0) {
-                    System.out.println("¡Has sido derrotado!");
-                    playerDefeated = true;
-                }
-            }
-        }
-        
-        System.out.println("La batalla ha terminado.");
-    }
+		System.out.println("ENEMIGO: "+agent.getName());
+
+		boolean playerDefeated = false;
+		boolean enemyDefeated = false;
+
+		if(!playerDefeated && !enemyDefeated) {
+			int playerDamage = player.getStrength();
+			int enemyDamage = agent.getStrength();
+
+			// Turno del jugador
+			//el Math.max devuelve el valor máximo entre los dos valores, para que no devuelva salud negativa
+			int enemyHP = Math.max(0, agent.getHealth() - playerDamage);
+			agent.setHealth(enemyHP);
+			System.out.println(player.getName() + " ataca al enemigo y causa " + playerDamage + " de daño.");
+			System.out.println("El enemigo tiene " + enemyHP + " de salud.");
+
+			// ver si se murió el enemigo
+			if (enemyHP <= 0) {
+				System.out.println("¡El enemigo ha sido derrotado!");
+				enemyDefeated = true;
+			} else {
+				// Turno del enemigo
+				int playerHP = Math.max(0, player.getHealth() - enemyDamage);
+				player.setHealth(playerHP);
+				System.out.println("El enemigo te ataca y causa " + enemyDamage + " de daño.");
+				System.out.println("Tienes " + playerHP + " de salud.");
+
+				// Ver si se murió el jugador
+				if (playerHP <= 0) {
+					System.out.println("¡Has sido derrotado!");
+					playerDefeated = true;
+				}
+			}
+		}
+	}
 }
